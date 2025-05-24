917d060a088ce81cf5885e62c0a9aed36bab8c64
GabrielKuiawa <gabrielsantos77239@gmail.com>
2025-05-24 09:35:32 -0300

    refactor: remove unused index method and simplify user retrieval in UsersController

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -7,15 +7,9 @@ use Core\Http\Controllers\Controller;
 
 class UsersController extends Controller
 {
-    public function index(): void
-    {
-        $title = 'Home Page';
-        $this->render('home/index', compact('users', 'title'));
-    }
-
     public function getAllUsers(): void
     {
-        $users = User::all(['id', 'name', 'email', 'is_manager', 'profile_picture_url']);
+        $users = User::all();
 
         $result = [];
 
